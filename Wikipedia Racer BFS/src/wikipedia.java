a9d6ef038ee7615cfe77ceea83cc08665853f517
petermart <32779280+petermart@users.noreply.github.com>
2018-12-10 22:53:37 -0700

    Update wikipedia.java
    Added offline storage for quicker link access.

--- Wikipedia Racer BFS/src/wikipedia.java
+++ Wikipedia Racer BFS/src/wikipedia.java
@@ -1,7 +1,12 @@
 import java.util.*;
 import java.io.*;
+import java.nio.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.net.*;
 import java.time.*;
+
 public class wikipedia {
 	public static ArrayList<Node> queue = new ArrayList<Node>();
 	public static ArrayList<String> visited = new ArrayList<String>();
@@ -9,28 +14,33 @@ public class wikipedia {
 	public static String end;
 	public static boolean found = false;
 	public static String endPath = "";
+	public static String fileStoragePath = "wiki-storage";
 	
 	public static void main (String args[]) throws IOException{
-
+		
+		//Creates new Path for storage
+		File f = new File(fileStoragePath);
+		Path p = Paths.get(f.getAbsolutePath());
+		if (!Files.exists(p)) {
+            Files.createDirectories(p);
+        }
+		
+		//Getting user input
 		Scanner reader = new Scanner (System.in);
 		System.out.println("Enter begin:");
 		begin = reader.nextLine();
 		System.out.println("Enter end:");
 		end = reader.nextLine();
-		
 		System.out.println("Starting at " + begin);
 		System.out.println("Aiming for " + end);
 		
-		
-//		getPageLinks(begin);
+		//Starts timer
 		long startTime = System.currentTimeMillis();
+		
+		//Begin BFS
 		queue.add(new Node(begin, ""));
-		
-		//queue object should not be string, and should contain a previous pointer,
-		//so that it can find the way it points back to it.
-		
-		
-		while (found == false) {
+		while (found == false && queue.isEmpty()==false) {
+			
 			if (visited.contains(queue.get(0).pageName)) {
 				queue.remove(0);
 			}
@@ -49,14 +59,21 @@ public class wikipedia {
 				queue.remove(0);
 			}
 	
-		}
+		} // End BFS
+		
 		long endTime = System.currentTimeMillis();
+		
+		if (!found) {
+			System.out.println("Path not possible");
+			return;
+		}
+		
 		System.out.println();
 		System.out.println("Found " + end);
 		System.out.println(endPath);
 		System.out.println("Found in " + ((endTime-startTime)/(float)1000) + " seconds");
 		
-		//Print out path of queue
+		
 	}
 	
 	public static String getPage(String pageTitle) throws IOException {
@@ -80,22 +97,14 @@ public class wikipedia {
 	}
 	
 	public static void getPageLinks(Node node) throws IOException{
-		String page = getPage(node.pageName);
-		ArrayList<String> links = new ArrayList<String>();
-		while (page.contains("[[")) {
-			page = page.substring(page.indexOf("[["));
-			int endex = page.indexOf("]]");
-			
-			String link = page.substring(0, endex+2);
-			if (link.contains(":")) {
-				page=page.substring(endex+2);
-			}
-			else {
-				if (link.contains("|")) {
-					link = link.substring(0, link.indexOf("|"))+"]]";
-				}
-				link = link.substring(2, link.length()-2);
-				//System.out.println(link);
+		File f = new File(fileStoragePath+"/"+node.pageName + ".txt");
+		
+		
+		if (f.exists()) { // Checking links from offline system
+			Scanner in = new Scanner(f);
+			ArrayList<String> links = new ArrayList<String>();
+			while (in.hasNextLine()) {
+				String link = (in.nextLine());
 				links.add(link);
 				if (!visited.contains(link)) {
 					queue.add(new Node(link, node.path+"->"+node.pageName));
@@ -104,11 +113,39 @@ public class wikipedia {
 					found = true;
 					endPath = node.path+"->"+node.pageName+"->"+link;
 				}
-				page = page.substring(endex+2);
+			}	
+			in.close();
+		} else { // Getting links from online and save to offline
+			PrintWriter out = new PrintWriter(f);
+			String page = getPage(node.pageName);
+			ArrayList<String> links = new ArrayList<String>();
+			while (page.contains("[[")) { // Parsing page for links in [[]]
+				page = page.substring(page.indexOf("[["));
+				int endex = page.indexOf("]]");
+				
+				String link = page.substring(0, endex+2);
+				if (link.contains(":")) {
+					page=page.substring(endex+2);
+				}
+				else {
+					if (link.contains("|")) {
+						link = link.substring(0, link.indexOf("|"))+"]]";
+					}
+					link = link.substring(2, link.length()-2);
+					out.println(link);
+					links.add(link);
+					if (!visited.contains(link)) {
+						queue.add(new Node(link, node.path+"->"+node.pageName));
+					}
+					if (link.toLowerCase().trim().equals(end.toLowerCase().trim())) {
+						found = true;
+						endPath = node.path+"->"+node.pageName+"->"+link;
+					}
+					page = page.substring(endex+2);
+				}
+				
 			}
-			
+			out.close();
 		}
-		
-		
 	}
 }
